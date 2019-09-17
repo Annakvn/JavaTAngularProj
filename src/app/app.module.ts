15911544f2bdb95aef2be4226ba74173a3f76512
Anna Kovneva <kovnevaa@yahoo.com>
2019-09-17 01:21:59 -0700

    creating routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProductAddComponent } from './product-add/product-add.component';
+import { ProductGetComponent } from './product-get/product-get.component';
+import { ProductEditComponent } from './product-edit/product-edit.component';
+import { AppRoutingModule } from './app-routing.module';
+import { SlimLoadingBarModule} from 'ng2-slim-loading-bar';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductAddComponent,
+    ProductGetComponent,
+    ProductEditComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    SlimLoadingBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
